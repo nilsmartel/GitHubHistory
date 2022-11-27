9b841c1369ce0b4885b471a2cf4a196f58310ca5
Nils Martel <nilsmartel@yahoo.de>
2022-11-27 14:22:13 +0100

    Write script to accumulate all commits into one table

--- graph.py
+++ graph.py
@@ -3,7 +3,6 @@ from multiprocessing import Pool
 import utils
 from sys import argv
 import os
-import numpy as np
 import pandas as pd
 
 
@@ -57,8 +56,7 @@ def get_csv_paths():
 """
 makes all informations inside a linecount-csv relative to it's previous position
 """
-def diff_csv_info(path):
-    df = pd.read_csv(path, sep=";")
+def diff_csv_info(df):
     # sort by unixtimestamp of commit date
     df.sort_values("unix")
 
@@ -82,9 +80,61 @@ def diff_csv_info(path):
                 diff = v - p
                 ndf[k].append(diff)
 
-
+    return pd.DataFrame.from_dict(ndf)
 
 
 # create thread pool for
 # downloading and organizing repositories.
 pool = Pool(32)
+
+dataframes = pool.map(diff_csv_info,
+    pool.map(lambda path: pd.read_csv(path, sep=";"),
+                          get_csv_paths())
+                      )
+
+# next merge all dataframes into one by concatenation
+
+def concat(frames):
+    b = dict()
+    keys = frames[0].keys()
+    for k in keys:
+        b[k] = []
+
+    for f in frames:
+        for k in keys:
+            b[k].append(f[f])
+
+    return pd.DataFrame.from_dict(b)
+
+df = concat(dataframes)
+df.sort_values("unix")
+
+
+# now, undo diffing
+
+def undiff(df):
+    b = dict()
+    acc = dict()
+
+    keys = df.keys()
+    for k in keys:
+        b[k] = []
+        if k not in ["repo", "commit", "date", "unix"]:
+            acc[k] = 0
+
+    rows = df.iterrows()
+
+    for _,row in rows:
+        for k in keys:
+            if k not in acc:
+                b[k].append(row[k])
+            else:
+                # else sum with acc
+                acc[k] += row[k]
+                b[k].append(acc[k])
+
+    newdf = pd.DataFrame.from_dict(b)
+    newdf.sort_values("unix")
+    return newdf
+
+df = undiff(df)
